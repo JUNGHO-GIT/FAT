3f3e21e68cb3f54f42e4408aa8d3ad65a613fa4f
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-08-22 22:15:13 +0900

    Update lang.ts

--- utils/lang.ts
+++ utils/lang.ts
@@ -18,13 +18,13 @@ export const languanges: LanguageConfig[] = [{
   baseUrl: "https://www.fatsecret.kr",
   menuUrl: "https://www.fatsecret.kr/칼로리-영양소",
   searchUrl: "https://www.fatsecret.kr/칼로리-영양소/search",
-  otherSizes: "Other sizes:", 
-  caloriesPrefix: "kcal", 
-  measurementRegex: { 
-     calories: /칼로리:|kcal/g,  
-     carb: /탄수화물:|g/g,    
-     fat: /지방:|g/g, 
-     protein: /단백질:|g/g
+  otherSizes:  "기타 제공량",
+  caloriesPrefix: "칼로리:",
+  measurementRegex: {
+    calories: /칼로리:|kcal/g,
+    carb: /탄수화물:|g/g,
+    fat: /지방:|g/g,
+    protein: /단백질:|g/g,
   }
 }];
 
