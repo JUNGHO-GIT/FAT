8fc8fbcde2c1a3fdb5da529d1321abebe91f6b3c
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-08-22 21:59:17 +0900

    Update lang.ts

--- utils/lang.ts
+++ utils/lang.ts
@@ -6,43 +6,27 @@ interface LanguageConfig {
   otherSizes: string;
   caloriesPrefix: string;
   measurementRegex: {
-    carb: RegExp;
-    protein: RegExp;
-    fat: RegExp;
     calories: RegExp;
+    carb: RegExp;
+    fat: RegExp;
+    protein: RegExp;
   };
 }
 
-export const languanges: LanguageConfig[] = [
-  {
-    lang: "en",
-    baseUrl: "https://www.fatsecret.com",
-    menuUrl: "https://www.fatsecret.com/calories-nutrition",
-    searchUrl: "https://www.fatsecret.com/calories-nutrition/search",
-    otherSizes: "Other sizes:",
-    caloriesPrefix: "kcal",
-    measurementRegex: {
-      carb: /Carbs:|g/g,
-      protein: /Protein:|g/g,
-      fat: /Fat:|g/g,
-      calories: /Calories:|kcal/g,
-    },
+export const languanges: LanguageConfig[] = [{
+  lang: "ko",
+  baseUrl: "https://www.fatsecret.kr",
+  menuUrl: "https://www.fatsecret.kr/칼로리-영양소",
+  searchUrl: "https://www.fatsecret.kr/칼로리-영양소/search",
+  otherSizes: "Other sizes:", 
+  caloriesPrefix: "kcal", 
+  measurementRegex: { 
+     calories: /칼로리:|kcal/g,  
+     carb: /탄수화물:|g/g,    
+     fat: /지방:|g/g, 
+     protein: /프로틴:|g/g, 
   },
-  {
-    lang: "ko",
-    baseUrl: "https://www.fatsecret.kr",
-    menuUrl: "https://www.fatsecret.kr/칼로리-영양소",
-    searchUrl: "https://www.fatsecret.kr/칼로리-영양소/search",
-    otherSizes: "Other sizes:", 
-    caloriesPrefix: "kcal", 
-    measurementRegex: { 
-       calories: /칼로리:|kcal/g,  
-       carb: /탄수화물:|g/g,    
-       fat: /지방:|g/g, 
-       protein: /단백질:|g/g, 
-    },
-  },
-];
+}];
 
 export function getLang(langCode: string): LanguageConfig | null {
   const lang = languanges.filter((lg) => lg.lang === langCode)[0];
