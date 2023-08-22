26ceb52cd73298ac176b19d68cdf9a4971607634
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-08-22 20:58:31 +0900

    Update lang.ts

--- utils/lang.ts
+++ utils/lang.ts
@@ -6,45 +6,45 @@ interface LanguageConfig {
   otherSizes: string;
   caloriesPrefix: string;
   measurementRegex: {
-    carb: RegExp,
-    protein: RegExp,
-    fat: RegExp,
-    calories: RegExp
-  }
+    carb: RegExp;
+    protein: RegExp;
+    fat: RegExp;
+    calories: RegExp;
+  };
 }
 
 export const languanges: LanguageConfig[] = [
   {
-    lang: 'en',
-    baseUrl: 'https://www.fatsecret.com',
-    menuUrl: 'https://www.fatsecret.com/calories-nutrition',
-    searchUrl: 'https://www.fatsecret.com/calories-nutrition/search',
-    otherSizes: 'Other sizes:',
-    caloriesPrefix: 'kcal',
+    lang: "en",
+    baseUrl: "https://www.fatsecret.com",
+    menuUrl: "https://www.fatsecret.com/calories-nutrition",
+    searchUrl: "https://www.fatsecret.com/calories-nutrition/search",
+    otherSizes: "Other sizes:",
+    caloriesPrefix: "kcal",
     measurementRegex: {
       carb: /Carbs:|g/g,
       protein: /Protein:|g/g,
       fat: /Fat:|g/g,
-      calories: /Calories:|kcal/g
-    }
+      calories: /Calories:|kcal/g,
+    },
   },
   {
-    lang: 'ko',
-    baseUrl: 'https://www.fatsecret.kr',
-    menuUrl: 'https://www.fatsecret.kr/칼로리-영양소',
-    searchUrl: 'https://www.fatsecret.kr/칼로리-영양소/search',
-    otherSizes: '다른크기 :',
-    caloriesPrefix: 'kcal',
+    lang: "ko",
+    baseUrl: "https://www.fatsecret.kr",
+    menuUrl: "https://www.fatsecret.kr/칼로리-영양소",
+    searchUrl: "https://www.fatsecret.kr/칼로리-영양소/search",
+    otherSizes: "다른크기 :",
+    caloriesPrefix: "kcal",
     measurementRegex: {
       carb: /탄수화물:|g/g,
       protein: /단백질:|g/g,
       fat: /지방:|g/g,
-      calories: /칼로리:|kkal/g
-    }
-  }
-]
+      calories: /칼로리:|kcal/g,
+    },
+  },
+];
 
-export function getLang (langCode: string): LanguageConfig | null {
-  const lang = languanges.filter((lg) => lg.lang === langCode)[0]
-  return lang || null
+export function getLang(langCode: string): LanguageConfig | null {
+  const lang = languanges.filter((lg) => lg.lang === langCode)[0];
+  return lang || null;
 }
